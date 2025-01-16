605be7766438900dc15e7a7279948c820b8e8a2d
Richard Chadwick <richardchadp@gmail.com>
2025-01-16 20:20:00 +0100

    Haciendo la preparción inicial del proyecto, creando y configurando una home page (index.html), creado un primeros_pasos.md donde explico todo lo que hay que hacer para empezar el proyecto, los primeros pasos

--- mi_proyecto/mi_proyecto/settings.py
+++ mi_proyecto/mi_proyecto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mi-aplicacion',
+    'mi_aplicacion',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mi_proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ 'templates'],
+        'DIRS': [ BASE_DIR / 'mi_proyecto/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
